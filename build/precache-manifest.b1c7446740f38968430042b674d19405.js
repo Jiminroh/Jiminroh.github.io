ee73ac603ba91500953e041ba0e1fe25a35a4dbf
Jimin (Rogan) <94786383+Jiminroh@users.noreply.github.com>
2023-07-26 20:08:44 +0900

    Change favicon

---
+++ build/precache-manifest.b1c7446740f38968430042b674d19405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea5546f8853e9a9d69ee66de3132867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b3307f46270e501732",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ec6a48c823a29388728c",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "e9b3307f46270e501732",
+    "url": "/static/js/2.0a358f82.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0a358f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6a48c823a29388728c",
+    "url": "/static/js/main.0fa5890f.chunk.js"
+  },
+  {
+    "revision": "df209f5f416870c7cd25",
+    "url": "/static/js/runtime-main.2c99bafe.js"
+  }
+]);
